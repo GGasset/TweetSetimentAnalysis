f3293bc009685c7afe8ffb7642b1d616bb1b2f77
Germán <GassetGerman@gmail.com>
2023-04-15 15:38:50 +0200

    added database path added .db termination to .gitignore

--- variables.py
+++ variables.py
@@ -1,3 +1,4 @@
 train_dataset_path = './twitter_training.csv'
 cleaned_train_dataset_path = './cleaned_twitter_training.csv'
-preprocessed_train_dataset_path = './preprocessed_twitter_training.csv'
+
+train_database_path = './twitter_training.db'
