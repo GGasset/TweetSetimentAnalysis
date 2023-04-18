3619594353c9a1aed80a22fde691212d5d8e6766
Germán <GassetGerman@gmail.com>
2023-04-18 22:47:52 +0200

    finished training.py

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 import os
 from sqlite3 import connect, Cursor
 import tensorflow as tf
-from variables import train_database_path
+from variables import train_database_path, model_path
 from preprocess import clean_tweet
 from postprocessing import get_sentiment_cols, generate_training_data, output_to_sentiment
 
@@ -16,6 +16,8 @@ def main():
     try:
         unique_sentiments, sentiment_cols = get_sentiment_cols(db)
         model = generate_model(unique_sentiments)
+        train_model(model, generate_training_data(db, unique_sentiments, sentiment_cols))
+        save_model(model)
     finally:
         db.close()
 
@@ -24,12 +26,17 @@ def generate_model(unique_sentiments: list[str]) -> tf.keras.models.Sequential:
         tf.keras.layers.Dense(len(unique_sentiments), activation='sigmoid', input_shape=(len(unique_sentiments),))
     ])
 
+    if os.path.isfile(model_path):
+        model.load_weights(model_path)
+
     model.compile(optimizer='nadam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=tf.keras.metrics.BinaryAccuracy())
     return model
 
-def train_model(model: tf.keras.models.Sequential(), db: Cursor):
-    pass
-    #tweets = db.
+def train_model(model: tf.keras.models.Sequential, training_data: tf.Tensor):
+    model.fit(training_data)
+
+def save_model(model: tf.keras.models.Sequential) -> None:
+    model.save(model_path)
 
 if __name__ == '__main__':
     main()
