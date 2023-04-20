00edf0e56dde10778f94de9af10e1f6b6d09a91f
Germán <GassetGerman@gmail.com>
2023-04-20 22:07:52 +0200

    probably finished training.py

--- training.py
+++ training.py
@@ -1,11 +1,18 @@
 import os
+import time
 from sqlite3 import connect, Cursor
 import tensorflow as tf
+
 from variables import train_database_path, model_path
 from preprocess import clean_tweet
-from postprocessing import get_sentiment_cols, generate_training_data, output_to_sentiment
+from postprocessing import get_sentiment_cols, generate_training_data
 
 def main():
+    print('Warning!\nThis file is obsolete as the network is not able to create accurate predictions. See recurrent_training.py, it features one-hot encoding for tweets and a RNN')
+    seconds_of_warning = 10
+    for i in range(seconds_of_warning + 1):
+        print(f'File will be executed in {seconds_of_warning - i} seconds', end='\r')
+        time.sleep(1)
     if not os.path.isfile(train_database_path):
         print('First execute preprocess.py')
         quit(1)
@@ -22,19 +29,22 @@ def main():
     finally:
         db.close()
 
-def generate_model(unique_sentiments: list[str]) -> tf.keras.models.Sequential:
+def generate_model(unique_sentiments: list[tuple[str]]) -> tf.keras.models.Sequential:
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(len(unique_sentiments), activation='sigmoid', input_shape=(None, len(unique_sentiments)))
+        tf.keras.layers.InputLayer(input_shape=(None, len(unique_sentiments))),
+        #tf.keras.layers.Dense(len(unique_sentiments), activation='sigmoid', bias_initializer='ones'),
+        #tf.keras.layers.Dense(5, activation='sigmoid'),
+        tf.keras.layers.Dense(len(unique_sentiments), activation='sigmoid')
     ])
 
     if os.path.isfile(model_path):
         model.load_weights(model_path)
 
-    model.compile(optimizer='nadam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=tf.keras.metrics.BinaryAccuracy())
+    model.compile(optimizer='nadam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=[tf.keras.metrics.MeanSquaredError()])
     return model
 
 def train_model(model: tf.keras.models.Sequential, X: tf.Tensor, Y: tf.Tensor):
-    model.fit(X, Y)
+    model.fit(X, Y, batch_size=10 ** 4, epochs=1, use_multiprocessing=True)
 
 def save_model(model: tf.keras.models.Sequential) -> None:
     model.save(model_path)
