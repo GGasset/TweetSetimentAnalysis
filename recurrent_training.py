898b2e6e75e66ba5b29bb2b46d7c4b3fc808a22d
Germán <GassetGerman@gmail.com>
2023-04-21 15:50:41 +0200

    test version of recurrent_training.py established

--- recurrent_training.py
+++ recurrent_training.py
@@ -1,22 +1,40 @@
+import os
 from sqlite3 import connect, Cursor
 import numpy as np
 import tensorflow as tf
 
 from variables import train_database_path, recurrent_model_path
-from postprocessing import get_one_hot_encoded_training_data
+from postprocessing import get_sentiment_cols, get_vocabulary, get_one_hot_encoded_training_data
 
 def main():
     db: Cursor
     try:
         db = connect(train_database_path)
+        unique_sentiments, _ = get_sentiment_cols(db)
+        vocabulary = get_vocabulary(db)
+        model = generate_model()
+        X, Y = get_one_hot_encoded_training_data(db, unique_sentiments, vocabulary)
+        fit_model(model, X, Y)
     finally:
         db.close()
 
 def generate_model(unique_sentiments: list[tuple[str]], vocab_length: int) -> tf.keras.models.Sequential:
-    pass
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(None, None, vocab_length)),
+        tf.keras.layers.LSTM(100, return_sequences=True),
+        tf.keras.layers.LSTM(len(unique_sentiments) ** 4),
+        tf.keras.layers.Dense(len(unique_sentiments))
+    ])
+    
+    if os.path.isfile(recurrent_model_path):
+        model.load_weights(recurrent_model_path)
+        print('model loaded from disk')
+
+    model.compile(optimizer='Nadam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+    return model
 
 def fit_model(model: tf.keras.Sequential, X: list[np.ndarray], Y: list[list[list[str]]]):
-    pass
+    model.fit(X, Y, batch_size=16, workers=8, use_multiprocessing=True)
 
 if __name__ == '__main__':
     main()
