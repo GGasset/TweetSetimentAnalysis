f6a239133deee4cb9fce0d9688f50ea3f58d16b5
Germán <GassetGerman@gmail.com>
2023-04-25 16:40:12 +0200

    added model_path modified one_hot_model_path

--- variables.py
+++ variables.py
@@ -3,4 +3,5 @@ cleaned_train_dataset_path = './cleaned_twitter_training.csv'
 
 train_database_path = './twitter_training.db'
 model_path = './sentiment_classifier.hdf5'
-recurrent_model_path = './one_hot_recurrent_sentiment_classifier.hdf5'
+one_hot_word_model_path = './one_hot_word_recurrent_sentiment_classifier.hdf5'
+one_hot_char_model_path = './one_hot_char_recurrent_sentiment_classifier.hdf5'
